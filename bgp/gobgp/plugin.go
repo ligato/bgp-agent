dce8ae45ca5a2c5c4389697a3487c935f4300dcd
Filip Gschwandtner <filip.gschwandtner@pantheon.tech>
2017-09-28 18:21:18 +0200

    enhanced lifecycle/API/documentation

--- bgp/gobgp/plugin.go
+++ bgp/gobgp/plugin.go
@@ -28,23 +28,23 @@ import (
 type Plugin struct {
 	Deps
 	server           *server.BgpServer
+	serverWatcher    *server.Watcher
 	watcherCallbacks []func(*bgp.ReachableIPRoute)
 	stopWatch        chan bool
 }
 
 // Deps combines all needed dependencies for Plugin struct. These dependencies should be injected into Plugin by using constructor's Deps parameter.
 type Deps struct {
-	local.PluginInfraDeps
-	SessionConfig *config.Bgp
-	PluginName    string
+	local.PluginInfraDeps             // inject
+	SessionConfig         *config.Bgp // inject
+	PluginName            string      // inject
 }
 
 func New(dependencies Deps) *Plugin {
 	return &Plugin{Deps: dependencies, watcherCallbacks: []func(*bgp.ReachableIPRoute){}}
 }
 
-//Init uses plugin config and starts gobgp with dedicated goroutine for watching gobgp and forwarding best path reachable ip routes to registered watchers.
-//After start of gobgp session, known neighbors from configuration are added to gobgp server.
+//Init creates the gobgp server and checks if needed SessionConfig was injected and fails if it is not.
 func (plugin *Plugin) Init() error {
 	plugin.Log.Debug("Init goBgp plugin")
 	if plugin.SessionConfig == nil {
@@ -52,8 +52,17 @@ func (plugin *Plugin) Init() error {
 		return fmt.Errorf("Can't init GoBGP plugin without configuration")
 	}
 	plugin.server = server.NewBgpServer()
-	go plugin.server.Serve()
 
+	return nil
+}
+
+// AfterInit starts gobgp with dedicated goroutine for watching gobgp and forwarding best path reachable ip routes to registered watchers.
+// After start of gobgp session, known neighbors from configuration are added to gobgp server.
+// Due to fact that AfterInit is called once Init() of all plugins have returned without error, other plugins can be registered watchers
+// from the start of gobgp server if they call this plugin's WatchIPRoutes() in their Init(). In this way they won't miss any information
+// forwarded to registered watchers just because they registered too late.
+func (plugin *Plugin) AfterInit() error {
+	go plugin.server.Serve()
 	if err := plugin.startSession(); err != nil {
 		return err
 	}
@@ -61,8 +70,8 @@ func (plugin *Plugin) Init() error {
 		return err
 	}
 	plugin.stopWatch = make(chan bool, 1)
-	w := plugin.server.Watch(server.WatchBestPath(true))
-	go plugin.watchChanges(w)
+	plugin.serverWatcher = plugin.server.Watch(server.WatchBestPath(true))
+	go plugin.watchChanges(plugin.serverWatcher)
 
 	return nil
 }
@@ -73,7 +82,6 @@ func (plugin *Plugin) watchChanges(watcher *server.Watcher) {
 		select {
 		case <-plugin.stopWatch:
 			plugin.Log.Debug("Stop Watching ", plugin.PluginName)
-			watcher.Stop()
 			return
 		case ev := <-watcher.Event():
 			switch msg := ev.(type) {
@@ -100,18 +108,23 @@ func (plugin *Plugin) watchChanges(watcher *server.Watcher) {
 	}
 }
 
-//Close performs clean up and final close
+//Close stops dedicated goroutine for watching gobgp. Then stops watcher provider by gobgp server and finally stops that gobgp server itself.
 func (plugin *Plugin) Close() error {
 	plugin.Log.Info("Closing goBgp plugin ", plugin.PluginName)
 	close(plugin.stopWatch)
+	plugin.serverWatcher.Stop()
 	return plugin.server.Stop()
 }
 
-//WatchIPRoutes subscribes consumer to notifications for any new learned IP-based routes
-func (plugin *Plugin) WatchIPRoutes(subscriber string, callback func(*bgp.ReachableIPRoute)) error {
+//WatchIPRoutes subscribes consumer to notifications for any new learned IP-based routes.
+//Subscription is not retroactive, that means that any IP-based routes learned in the past are not send to new subscribers.
+//This also means that if you want be notified of all learned IP-based routes, you must subscribe before calling of
+//AfterInit(). In case of external(=not other plugin started with this plugin) subscribers this means before plugin start.
+//However, late subscribers are permitted (no error will be returned), but they can miss some learned IP-based routes.
+func (plugin *Plugin) WatchIPRoutes(subscriber string, callback func(*bgp.ReachableIPRoute)) (Subscription, error) {
 	plugin.Log.Infof("Subscriber %s registering for watching of IPRoutes in %s.", subscriber, plugin.PluginName)
 	plugin.watcherCallbacks = append(plugin.watcherCallbacks, callback)
-	return nil
+	return &subscription{}, nil
 }
 
 //startSession starts session on already running goBGP server
@@ -132,5 +145,16 @@ func (plugin *Plugin) addKnownNeighbors() error {
 		}
 	}
 
+	return nil
+}
+
+type Subscription interface {
+	Close() error
+}
+
+type subscription struct{}
+
+func (s *subscription) Close() error {
+	//TODO add removal from Plugin.watcherCallbacks
 	return nil
 }
